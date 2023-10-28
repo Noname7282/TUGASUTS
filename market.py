e27e1013998fd18fc286c2c3e12789a5bd015743
IqsalIN <147210231+Noname7282@users.noreply.github.com>
2023-10-29 00:55:32 +0700

    Add files via upload

---
+++ market.py
@@ -0,0 +1,20 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('stock_market.sav', 'rb'))
+
+st.title('Stock Price Prediction App')
+Date = st.number_input('Input Date)')
+Open = st.number_input('Input Harga')
+High = st.number_input('Input Harga Tertinggi')
+Low = st.number_input('Input Harga Terendah')
+Last = st.number_input('Input Harga Terakhir)')
+Close = st.number_input('Input Harga Penutupan')
+
+predict = ''
+
+if st.button('Predict'):
+    predict = model.predict(
+        [[Date, Open, High, Low, Last, Close]]
+        )
+    st.write ('Turnover : ', predict)
